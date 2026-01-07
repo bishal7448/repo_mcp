c72b059a5399d51f36e5493b6c1a29ae3d62e438
Bishal Saha <saha.bishal7676@gmail.com>
2026-01-07 21:03:26 +0530

    feat: Introduce Google Gemini embedding

--- app/llm/embedding.py
+++ app/llm/embedding.py
@@ -1,15 +1,16 @@
-from llama_index.embeddings.nebius import NebiusEmbedding
+import os
+from dotenv import load_dotenv
+from llama_index.embeddings.google import GeminiEmbedding
 from app.core.config import settings
 
 class EmbeddingProvider:
     def __init__(self):
-        self.embedding = NebiusEmbedding(
-            api_key=settings.NEBIUS_API_KEY,
-            model_name="BAAI/bge-en-icl", # TODO: change model
-            embed_batch_size=10
+        self.embedding = GeminiEmbedding(
+            model_name="models/text-embedding-004",
+            api_key="AIzaSyCKZTLLFExGXPrRhIE1uZurHrkp3ZTki6Q",
         )
 
     def get_embedding(self):
         return self.embedding
-    
+
 embedding_provider = EmbeddingProvider()
